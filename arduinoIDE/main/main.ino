71882facdb1de276ce41300d68ee4d4723ae359d
jonathanTorf <jonatantorf@gmail.com>
2026-03-23 14:09:33 +0200

    a

--- arduinoIDE/main/main.ino
+++ arduinoIDE/main/main.ino
@@ -1,1 +1,18 @@
+#include "sd.h"
 
+sdCard sdc(53);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("program starting");
+
+  sdc.begin();
+  Serial.println(sdc.readLine("test.txt", 0));
+  sdc.writeLine("test.txt", 2, "deez nuts");
+  Serial.println(sdc.readLine("test.txt", 2));
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
